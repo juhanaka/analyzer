d65f51327ae62609c7d0d5472e57e95cf0d3295e
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2013-12-08 19:57:10 +0200

    File structure changes

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "analyzer.settings"***REMOVED***
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings"***REMOVED***
 
     from django.core.management import execute_from_command_line
 
